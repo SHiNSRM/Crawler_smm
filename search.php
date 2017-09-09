944cddf1678936b861c38ba10710f9f8862eaeba
SHiNSRM <shin.matsu36@gmail.com>
2017-09-09 23:13:44 +0900

    crawler

---
+++ search.php
@@ -0,0 +1,61 @@
+<form method="POST" enctype="multipart/form-data" action="search.php">
+  <input type="text" name="a"/>
+  <input type="submit" value="送信" /><br><br>
+</form>
+<?php
+function gkwg($word){
+$url = "http://www.google.com/complete/search?hl=ja&output=toolbar&ie=utf_8&oe=utf_8&q=".urlencode($word);
+$gkwg_xml = simplexml_load_file($url);
+ if(isset( $gkwg_xml->CompleteSuggestion ) ){
+  foreach ($gkwg_xml->CompleteSuggestion as $completeSuggestion) {
+   $suggest_word_array[] = $completeSuggestion->suggestion->attributes()->data;
+  }
+  return $suggest_word_array;
+ }else{
+  return false;
+ }
+}
+function get_noun($txet){
+  $exe_path = 'C:/"Program Files (x86)"/MeCab/bin/mecab.exe';
+	$descriptorspec = array(
+		0 => array("pipe", "r"),
+		1 => array("pipe", "w")
+	);
+	$process = proc_open($exe_path, $descriptorspec, $pipes);
+	if (is_resource($process)) {
+		fwrite($pipes[0], $txet);
+		fclose($pipes[0]);
+		$tmp = stream_get_contents($pipes[1]);
+		fclose($pipes[1]);
+		proc_close($process);
+	}
+	$result=[];
+	$result = explode("\r\n", $tmp);
+  $word_list=[];
+  foreach($result as $val) {
+  	$tmp=explode(",",$val);
+  	$tmp=explode("\t",$tmp[0]);
+  	if (@$tmp[1]=='名詞') {
+      $word_list[]=$tmp[0];
+  	}
+  }
+  $str="";
+  foreach($word_list as $val) {
+    $str.=$val." ";
+  }
+  return $str;
+}
+
+
+if(isset($_POST["a"])){
+$word=get_noun($_POST["a"]);
+echo $word."<br><br>";
+//$word=$_POST["a"]." ";
+$ar_gkwg=gkwg($word);
+if($ar_gkwg){
+	foreach($ar_gkwg as $gkwg){
+		echo $gkwg.'<br>';
+	}
+}
+}
+?>
